69dfd55055778bf42c25025e614fae47c4f9e5f7
lanjerry <19921oo2my>
2020-03-02 01:50:00 +0800

    refactor：更新sql文件

--- sql/lanjerry.sql
+++ sql/lanjerry.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 127.0.0.1
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 80012
- Source Host           : 127.0.0.1:3306
+ Source Server Version : 80018
+ Source Host           : localhost:3306
  Source Schema         : lanjerry
 
  Target Server Type    : MySQL
- Target Server Version : 80012
+ Target Server Version : 80018
  File Encoding         : 65001
 
- Date: 27/12/2019 11:51:33
+ Date: 02/03/2020 01:49:22
 */
 
 SET NAMES utf8mb4;
@@ -37,7 +37,7 @@ CREATE TABLE `sys_log`  (
   `exception_msg` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '异常信息',
   `created_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 357899 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统日志表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 358025 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统日志表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_permission
@@ -60,7 +60,7 @@ CREATE TABLE `sys_permission`  (
   `creator_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
   `created_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 34 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统权限表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 37 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统权限表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_permission
@@ -89,6 +89,10 @@ INSERT INTO `sys_permission` VALUES (21, '停用系统权限', 15, 2, NULL, NULL
 INSERT INTO `sys_permission` VALUES (22, '日志管理', 1, 1, 'log', 'sys/log', 'sys:log', 'log', 40, 1, 0, 0, 1, '管理员', '2019-09-05 18:01:30');
 INSERT INTO `sys_permission` VALUES (23, '分页查询系统日志列表', 22, 2, NULL, NULL, 'sys:log:page', '#', 10, 1, 0, 0, 1, '管理员', '2019-09-05 18:01:42');
 INSERT INTO `sys_permission` VALUES (24, '删除系统日志', 22, 2, NULL, NULL, 'sys:log:remove', '#', 20, 1, 0, 0, 1, '管理员', '2019-12-27 11:26:40');
+INSERT INTO `sys_permission` VALUES (34, '系统工具', 0, 1, 'tool', '', 'tool', 'tool', 99, 1, 0, 0, 1, '管理员', '2020-02-11 16:18:11');
+INSERT INTO `sys_permission` VALUES (35, '系统接口', 34, 1, 'swagger', 'tool/swagger', '', 'swagger', 99, 1, 0, 0, 1, '管理员', '2020-02-11 16:20:05');
+INSERT INTO `sys_permission` VALUES (36, '代码生成', 34, 1, 'gen', 'tool/gen', '', 'code', 10, 1, 0, 0, 1, '管理员', '2020-02-11 16:54:24');
+INSERT INTO `sys_permission` VALUES (37, '表单构建', 34, 1, 'build', 'tool/build', '', 'build', 99, 1, 0, 0, 1, '管理员', '2020-02-11 16:56:56');
 
 -- ----------------------------
 -- Table structure for sys_role
@@ -103,7 +107,7 @@ CREATE TABLE `sys_role`  (
   `creator_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
   `created_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 26 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统角色表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 223 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统角色表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_role
@@ -122,7 +126,7 @@ CREATE TABLE `sys_role_permission`  (
   `creator_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
   `created_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 80 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统角色权限表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 85 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统角色权限表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_role_permission
@@ -170,12 +174,12 @@ CREATE TABLE `sys_user`  (
   `creator_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
   `created_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 65 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统用户表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 66 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统用户表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES (1, 'admin', 'aaa42296669b958c3cee6c0475c8093e', '管理员', 1, '123456@qq.com', '15017919916', '', 1, '', NULL, 0, 1, '管理员', '2019-04-11 16:01:33');
+INSERT INTO `sys_user` VALUES (1, 'admin', 'aaa42296669b958c3cee6c0475c8093e', '管理员', 1, '123456@qq.com', '15017919916', '', 1, '127.0.0.1', '2020-03-02 01:48:54', 0, 1, '管理员', '2019-04-11 16:01:33');
 
 -- ----------------------------
 -- Table structure for sys_user_role
@@ -189,11 +193,62 @@ CREATE TABLE `sys_user_role`  (
   `creator_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
   `created_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 45 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统用户角色表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 48 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统用户角色表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_user_role
 -- ----------------------------
-INSERT INTO `sys_user_role` VALUES (1, 1, 1, 1, '管理员', '2019-04-16 19:22:39');
+INSERT INTO `sys_user_role` VALUES (48, 1, 1, 1, '管理员', '2020-02-15 01:23:05');
+
+-- ----------------------------
+-- Table structure for tool_gen
+-- ----------------------------
+DROP TABLE IF EXISTS `tool_gen`;
+CREATE TABLE `tool_gen`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '表编号',
+  `table_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '表名称',
+  `table_comment` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '表描述',
+  `class_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '实体类',
+  `tpl_function` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '模板功能',
+  `package_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '生成包路径',
+  `module_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '生成模块名',
+  `business_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '生成业务名',
+  `function_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '生成功能名',
+  `function_author` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '生成功能作者',
+  `remark` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
+  `creator_id` int(11) NULL DEFAULT NULL COMMENT '创建人id',
+  `creator_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
+  `created_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `updated_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 46 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '代码生成业务表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for tool_gen_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `tool_gen_detail`;
+CREATE TABLE `tool_gen_detail`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '字段编号',
+  `table_id` int(11) NOT NULL COMMENT '表编号',
+  `column_name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '列名称',
+  `column_comment` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '列描述',
+  `column_type` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '列类型',
+  `java_type` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'JAVA类型',
+  `java_field` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'JAVA字段名',
+  `pk_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否主键 0.否 1.是',
+  `increment_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否自增 0.否 1.是',
+  `required_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否必填 0.否 1.是',
+  `only_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否唯一 0.否 1.是',
+  `form_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否表单字段 0.否 1.是',
+  `list_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否列表字段 0.否 1.是',
+  `query_flag` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否查询字段 0.否 1.是',
+  `query_type` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '查询方式',
+  `html_type` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '显示类型',
+  `creator_id` int(11) NULL DEFAULT NULL COMMENT '创建人id',
+  `creator_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
+  `created_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `updated_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 541 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '代码生成业务明细表' ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
